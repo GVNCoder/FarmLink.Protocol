a7c1050c2219bd459ae4625e1a780167a682355f
GVNCoder <dmitrievsvp@gmail.com>
2026-06-05 17:02:04 +0300

    Refactor Message to record struct for simplicity

--- src/FarmLink.Protocol/Specification/Types/Message.cs
+++ src/FarmLink.Protocol/Specification/Types/Message.cs
@@ -1,10 +1,16 @@
 ﻿namespace FarmLink.Protocol.Specification.Types;
 
-public ref struct Message
-{
-    public ProtocolVersion ProtocolVersion;
-    public short DeviceID;
-    public int MessageID;
-    public MessageType MessageType;
-    public ReadOnlySpan<byte> Payload;
-}
+/// <summary>
+/// Represents a FarmLink Protocol Message.
+/// </summary>
+/// <param name="ProtocolVersion">The version of the protocol used for this message.</param>
+/// <param name="DeviceID">The unique identifier of the device sending or receiving the message.</param>
+/// <param name="MessageID">The unique identifier of the message, used for tracking and correlation.</param>
+/// <param name="MessageType">The type of the message, indicating its purpose and how it should be processed.</param>
+/// <param name="Payload">The content of the message, which can vary in structure and meaning based on the MessageType.</param>
+public record struct Message(
+    ProtocolVersion ProtocolVersion,
+    short DeviceID,
+    int MessageID,
+    MessageType MessageType,
+    byte[] Payload);
